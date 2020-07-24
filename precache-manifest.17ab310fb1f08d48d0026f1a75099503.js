56c0f75e2c74fe3b3b0666e872cb41f78b0b4f06
jiwothur <jiwothur@naver.com>
2020-07-24 13:03:02 +0900

    Updates

---
+++ precache-manifest.17ab310fb1f08d48d0026f1a75099503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc57e130a9e69b7a1ea1090ed9e8b4ba",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "1d107a738d6b826a9b94",
+    "url": "/movie_app/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d107a738d6b826a9b94",
+    "url": "/movie_app/static/js/main.1a45e10d.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
